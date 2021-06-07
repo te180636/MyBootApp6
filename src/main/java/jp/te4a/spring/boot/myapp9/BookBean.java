f98138bbc4d58e2825d203953fa5b864a2e755ed
SanNomiya52411 <right52411@gmail.com>
2021-06-07 10:48:16 +0900

    kadai9

---
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
@@ -0,0 +1,25 @@
+package jp.te4a.spring.boot.myapp9;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookBean {
+@Id
+@GeneratedValue
+	Integer id;
+	@Column(nullable = false)
+	String title;
+	String writter;
+	String publisher;
+	Integer price;
+}
